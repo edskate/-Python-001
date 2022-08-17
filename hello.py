3de2cfecc09189cb61f25a82e902358dc99bfebf
edilson gomes de lima <edskateicevida@gmail.com>
2022-08-17 04:58:15 -0300

    python 05

--- hello.py
+++ hello.py
@@ -6,3 +6,14 @@ age = 25
 print("hello my name is %s and I am %d years old" % (name, age))
 
 
+
+from os import name
+name='edilson'
+age=48
+print("hello my name is %s and I am %d yers old" % (name, age))
+
+nome ='1 bruno '
+
+
+nomes =[nome,nome, nome]
+
